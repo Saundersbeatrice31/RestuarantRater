76ef2aa5ab5279453d0e10ed31c5adf69c777ba3
Beatrice Saunders <saundersbeatrice31@gmail.com>
2021-08-09 13:52:28 -0400

    Add project files.

---
+++ RestuarantRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestuarantRater.Startup))]
+namespace RestuarantRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
